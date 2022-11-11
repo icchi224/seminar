729875ade405461744118e7ea6c86f04027778c7
ichi <hiroaki.i.6501@gmail.com>
2022-11-11 14:35:39 +0900

    11/11

--- プログラム/camface.py
+++ プログラム/camface.py
@@ -1,56 +1,41 @@
-# -*- coding: utf-8 -*-
-import time
-import picamera
-import cv2 as cv
+import numpy as np
+import cv2
 
-fn = 'my_pic.jpg'
+# 学習済みモデルを読み込む
+faceCascade = cv2.CascadeClassifier('/home/pi/face/model/opencv-master/data/haarcascades/haarcascade_frontalface_default.xml')
+# faceCascade = cv2.CascadeClassifier('/home/pi/face/model/opencv-master/data/haarcascades/haarcascade_eye.xml')
 
-# カメラ初期化
-with picamera.PiCamera() as camera:
-	# 解像度の設定
-	camera.resolution = (512, 384)
-	# 撮影の準備
-	camera.start_preview()
-	# 準備している間、少し待機する
-	time.sleep(2)
-	# 撮影して指定したファイル名で保存する
-	camera.capture(fn)
+# カメラで動画を撮影する カメラ1台の場合は引数に0 or -1を設定する
+cap = cv2.VideoCapture(0)
+cap.set(3,640) # 横幅を設定 
+cap.set(4,480) # 縦幅を設定
+while True:
+    
+    # フレーム毎にキャプチャする
+    ret, img = cap.read()
 
-	# 撮影した写真を読み込む
-	img = cv.imread(fn)
+    # 顔検出の負荷軽減のために、キャプチャした画像をモノクロにする
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # 顔検出のパラメータの設定
+    faces = faceCascade.detectMultiScale(
+        gray,     
+        scaleFactor=1.2,
+        minNeighbors=5,     
+        minSize=(20, 20)
+    )
+    # 顔検出時に四角い枠を表示
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
 
-	# 顔検出の処理効率化のために、写真の情報量を落とす（モノクロにする）
-	grayimg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # imshow関数で結果を表示する
+    cv2.imshow('video',img)
 
-	# 顔検出のための学習元データを読み込む
-	face_cascade = cv.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
-	# 目検出のための学習元データを読み込む
-	eye_cascade = cv.CascadeClassifier('haarcascades/haarcascade_eye.xml')
-	# 顔検出を行う
-	facerect = face_cascade.detectMultiScale(grayimg, scaleFactor=1.2, minNeighbors=2, minSize=(1, 1))
-	# 目検出を行う
-	eyerect = eye_cascade.detectMultiScale(grayimg)
+    # ESCが押されたら終了する
+    k = cv2.waitKey(30) & 0xff
+    if k == 27: 
+        break
 
-	print(facerect)
-	print(eyerect)
-
-	# 顔を検出した場合
-	if len(facerect) > 0:
-		# 検出した場所すべてに赤色で枠を描画する
-		for rect in facerect:
-			cv.rectangle(img, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), (0, 0, 255), thickness=3)
-
-	# 目を検出した場合
-	if len(eyerect) > 0:
-		# 検出した場所すべてに緑色で枠を描画する
-		for rect in eyerect:
-			cv.rectangle(img, tuple(rect[0:2]), tuple(rect[0:2]+rect[2:4]), (0, 255, 0), thickness=3)
-
-	# 結果の画像を表示する
-	cv.imshow('camera', img)
-	# 結果を書き出す
-	cv.imwrite(fn, img)
-	# 何かキーが押されるまで待機する
-	cv.waitKey(0)
-	# 表示したウィンドウを閉じる
-	cv.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
